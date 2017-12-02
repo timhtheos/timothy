b120fc9e5af47d920a47f05cfbe75254f7a989cb
timhtheos <timothy.escopete@monimedia.com>
2017-12-02 23:54:09 +0800

    Preparation.

--- js/script.js
+++ js/script.js
@@ -5,18 +5,23 @@ $(document).ready(function() {
   });
 
   $comment_div = $("#disqus-recent-comments");
-  $.get("https://disqus.com/api/3.0/forums/listPosts.json?forum=timhtheos&limit=10&related=thread&api_key=mARVtOgZo3Gjz2whc6CPuR1eQZywsACQkvsKLRc2ljXL4A0hia640WHJGIktiJ5Z", function(res, code) {
+  $.get("https://disqus.com/api/3.0/forums/listPosts.json?forum=timhtheos&limit=20&related=thread&api_key=mARVtOgZo3Gjz2whc6CPuR1eQZywsACQkvsKLRc2ljXL4A0hia640WHJGIktiJ5Z", function(res, code) {
     //Good response?
     if(res.code === 0) {
       var result = "";
       for(var i=0, len=res.response.length; i<len; i++) {
         var post = res.response[i];
         console.dir(post);
-        var html = "<div class='comment'>";
-        html += "<img src='" + post.author.avatar.small.permalink + "'>";
-        html += "<a href='"+ post.author.profileUrl + "'>" + post.author.name + "</a>";
-        html += "<p>"+post.raw_message+"</p>";
-        html += "<p class='postRef'>Posted at " + post.createdAt + " on <a href='"+ post.thread.link + "'>" + post.thread.title + "</a></p>";
+        var html = "<div class='disqus-comment'>";
+        html += "<h3 class='disqus-comment-title'><a href='"+ post.thread.link + "'>" + post.thread.title + "</a></h3>";
+        html += "<div class='disqus-comment-wrapper'>";
+        html += "<div class='disqus-comment-image'><img src='" + post.author.avatar.small.permalink + "'></div>";
+        html += "<div class='disqus-comment-message-wrapper'>";
+        html += "<div class='disqus-comment-author'><i class='fa fa-user' aria-hidden='true'></i><a target='_blank' href='"+ post.author.profileUrl + "'>" + post.author.name + "</a></div>";
+        html += "<div class='disqus-comment-date'><i class='fa fa-clock-o' aria-hidden='true'></i>" + post.createdAt + "</div>";
+        html += "<p class='disqus-comment-message'>"+post.raw_message+"</p>";
+        html += "</div>";
+        html += "</div>";
         html += "</div>";
 
         result+=html;
