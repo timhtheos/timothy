d1f90b162ff8387fbbb4269ba1fdbdcd47fbec2c
timhtheos <timothy.escopete@monimedia.com>
2017-12-03 15:27:06 +0800

    Fix website styling, before the time.

--- js/script.js
+++ js/script.js
@@ -11,18 +11,26 @@ $(document).ready(function() {
       var result = "";
       for(var i=0, len=res.response.length; i<len; i++) {
         var post = res.response[i];
+        // var date = moment(post.createdAt).fromNow();
+        var date = moment(post.createdAt);
+        date = date.format("DD MMM YYYY");
+        var msg = post.raw_message;
+        var author = post.author.name;
+        author = author.substring(0,7);
         console.dir(post);
         var html = "<div class='disqus-comment'>";
-        html += "<h3 class='disqus-comment-title'><a href='"+ post.thread.link + "'>" + post.thread.title + "</a></h3>";
-        html += "<div class='disqus-comment-wrapper'>";
-        html += "<div class='disqus-comment-image'><img src='" + post.author.avatar.small.permalink + "'></div>";
-        html += "<div class='disqus-comment-message-wrapper'>";
-        html += "<div class='disqus-comment-author'><i class='fa fa-user' aria-hidden='true'></i><a target='_blank' href='"+ post.author.profileUrl + "'>" + post.author.name + "</a></div>";
-        html += "<div class='disqus-comment-date'><i class='fa fa-clock-o' aria-hidden='true'></i>" + post.createdAt + "</div>";
-        html += "<p class='disqus-comment-message'>"+post.raw_message+"</p>";
-        html += "</div>";
-        html += "</div>";
-        html += "</div>";
+        html +=    "  <h3 class='disqus-comment-title'><a href='"+ post.thread.link + "'>" + post.thread.title + "</a></h3>";
+        html +=    "  <div class='disqus-comment-wrapper'>";
+        html +=    "    <div class='disqus-comment-image'><img src='" + post.author.avatar.small.permalink + "'></div>";
+        html +=    "    <div class='disqus-comment-message-wrapper'>";
+        html +=    "      <div class='disqus-comment-author-wrapper'>";
+        html +=    "        <div class='disqus-comment-author'><i class='fa fa-user' aria-hidden='true'></i><a target='_blank' href='"+ post.author.profileUrl + "'>" + author + "</a></div>";
+        html +=    "        <div class='disqus-comment-date'><i class='fa fa-clock-o' aria-hidden='true'></i>" + date + "</div>";
+        html +=    "      </div>";
+        html +=    "      <div class='disqus-comment-message'>" + msg + "</div>";
+        html +=    "    </div>";
+        html +=    "  </div>";
+        html +=    "</div>";
 
         result+=html;
       }
